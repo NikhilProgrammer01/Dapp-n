fad98b796a3b670492a7de736d9589c29a9351c2
mehul455 <gmehul127@gmail.com>
2022-08-13 22:07:12 +0530

    dapp

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img  src="https://techcrunch.com/wp-content/uploads/2020/09/dfinity.jpg" className="App-logo" alt="logo" />
+       <p style={{marginRight:"750px"}}>Nikhil's first Dapp</p>
       </header>
     </div>
   );
